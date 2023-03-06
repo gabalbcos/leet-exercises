1788429ffd1f8d3518e9370880e4d3888aa4da0b
Gabriel Albuquerque <114111654+gabalbcos@users.noreply.github.com>
2023-03-06 19:50:21 -0300

    Add solution to problem 1539 - Kth Missing Positive Number

--- javascript.js
+++ javascript.js
@@ -5,3 +5,17 @@ var defangIPaddr = function(address) {
     const newStr = address.replace(reg, "[.]");
     return newStr;
 };
+
+//1539. Kth Missing Positive Number
+var findKthPositive = function(arr, k) {
+    let finalArray = [];
+    let i = 1;
+    while (finalArray.length +1 <= k){
+        if (arr.indexOf(i) === -1){
+            finalArray.push(i);
+            }
+            i++
+        }
+    console.log(finalArray);
+    return finalArray[k-1];
+};
